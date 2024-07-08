fd5102b9f5b7959859631f7fdd11fdf9a1abdb10
Philipp Schlüter <50487305+roink@users.noreply.github.com>
2024-07-08 20:49:22 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,47 @@
+const CACHE_NAME = 'travel-time-cache-v2';
+const urlsToCache = [
+    '/travel-time-loading-bar/',
+    '/travel-time-loading-bar/index.html',
+    '/travel-time-loading-bar/manifest.json',
+    '/travel-time-loading-bar/icon.png'
+];
+
 self.addEventListener('install', function(event) {
+    // Perform install steps
     event.waitUntil(
-        caches.open('travel-time-cache').then(function(cache) {
-            return cache.addAll([
-                '/travel-time-loading-bar/',
-                '/travel-time-loading-bar/index.html',
-                '/travel-time-loading-bar/manifest.json',
-                '/travel-time-loading-bar/icon.png'
-            ]);
-        })
+        caches.open(CACHE_NAME)
+            .then(function(cache) {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
     );
 });
 
 self.addEventListener('fetch', function(event) {
     event.respondWith(
-        caches.match(event.request).then(function(response) {
-            return response || fetch(event.request);
+        caches.match(event.request)
+            .then(function(response) {
+                // Cache hit - return response
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            }
+        )
+    );
+});
+
+self.addEventListener('activate', function(event) {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then(function(cacheNames) {
+            return Promise.all(
+                cacheNames.map(function(cacheName) {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
         })
     );
 });
